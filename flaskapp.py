d41d144396b45798218716b9e66e7e976330d241
Don <don.miguel013@gmail.com>
2015-11-03 11:44:26 -0500

    add upload code

--- flaskapp.py
+++ flaskapp.py
@@ -2,9 +2,14 @@ import os
 from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = os.path.join(app.static_folder, 'imgs')
+ALLOWED_EXTENSIONS = set(['txt', 'bmp', 'png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -17,6 +22,29 @@ def serveStaticResource(resource):
 @app.route("/test")
 def test():
     return "<strong>It's Alive!</strong>"
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
     app.run()
