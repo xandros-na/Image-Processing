fb2044d27f2966310b10566aad716ff54c7c84fe
Don <don.miguel013@gmail.com>
2015-11-03 23:18:04 -0500

    made template for upload

--- flaskapp.py
+++ flaskapp.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
 from werkzeug import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
 
 ALLOWED_EXTENSIONS = set(['txt', 'bmp', 'png', 'jpg', 'jpeg'])
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 run_config = 'dev'
 app.config.from_pyfile('flaskapp.cfg')
 app.config['UPLOAD_FOLDER'] = os.path.join(app.static_folder, 'img')
+app.config['MAX_CONTENT_LENGTH'] = 512 * 1024
 
 @app.route('/<path:resource>')
 def serveStaticResource(resource):
@@ -32,20 +34,16 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file.close()
-            flash('success')
-            return redirect(url_for('image', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
+            try:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                file.close()
+                flash('success')
+                return redirect(url_for('image', filename=filename))
+            except RequestEntityTooLarge as e:
+                flash('data too large')
+                return redirect(url_for('upload_file'))
+           
+    return render_template('upload.html')
 if __name__ == '__main__':
     app.run()
