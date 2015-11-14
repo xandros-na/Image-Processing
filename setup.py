00b1606de1540a238b798609a6e870dc59a6140a
Don <don.miguel013@gmail.com>
2015-11-13 23:39:46 -0500

    update setup file with proper description etc

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
+setup(name='CP467 Image Processing Project',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+      description='Web app that implements filters, thinning and feature vectors',
+      author='Chunxiao Li and Don Miguel',
+      author_email='lixx8170@mylaurier.ca, migu0850@mylaurier.ca',
      install_requires=['Flask>=0.10.1', 'Pillow==2.6.1'],
      )
