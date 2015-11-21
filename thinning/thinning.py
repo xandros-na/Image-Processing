fe1e23c4be98c61b8ee3d4d09b1c27984195bfeb
Don <don.miguel013@gmail.com>
2015-11-20 20:27:13 -0500

    improve thinning performance

--- thinning/thinning.py
+++ thinning/thinning.py
@@ -1,51 +1,69 @@
 import copy
 from filters import get_matrix
+import time
 BLACK = 0
 WHITE = 255
 
 def zs_thin(img):
+    img=zero_padding(img)
     flag = 1
     processing = True
     while processing:
-        temp_pixel = copy.deepcopy(img.pixels)
+
         changed=False
-        for r in range(img.height):
-            for c in range(img.width):
+        # t1=time.time()
+        remove=[]
+        for r in range(1,img.height-1):
+            t1=time.time()
+            for c in range(1,img.width-1):
                 if img.pixels[r][c] == BLACK:
-                    matrix = get_matrix(r, c, img)
-                    if _zs_thin_conditions(matrix, flag):
-                        temp_pixel[r][c] = WHITE
+                    if _zs_thin_conditions(r,c,img, flag):
+                        remove.append([r,c])
                         changed=True
-        img.pixels = temp_pixel
+            print(time.time()-t1)
+        # print(time.time()-t1)
+        for i in remove:
+            img.pixels[i[0]][i[1]]=WHITE
         if changed is False:
             processing= False
         flag*=-1
 
+
     return img.pixels
 
+def zero_padding(img):
 
-def neighbor_check(matrix):
+    for i in range(img.height):
+        img.pixels[i]=[255]+img.pixels[i]
+        img.pixels[i].append(255)
+    img.height+=2
+    img.width+=2
+    img.pixels=[[255 for i in range(img.width)]]+img.pixels
+    img.pixels.append([255 for i in range(img.width)])
+
+    return img
+
+def neighbor_check(points):
     counter = 0
-    for i in matrix:
-        for j in i:
-            if j == BLACK:
-                counter += 1
+    for i in points:
+        if i==0:
+            counter+=1
 
     return 2 <= counter-1 <=6
 
-def _zs_thin_conditions(matrix, flag):
-    p2 = matrix[0][1]
-    p3 = matrix[0][2]
-    p4 = matrix[1][2]
-    p5 = matrix[2][2]
-    p6 = matrix[2][1]
-    p7 = matrix[2][0]
-    p8 = matrix[1][0]
-    p9 = matrix[0][0]
+def _zs_thin_conditions(r,c,img, flag):
+    p2 = img.pixels[r-1][c]
+    p3 = img.pixels[r-1][c+1]
+    p4 = img.pixels[r][c+1]
+    p5 = img.pixels[r+1][c+1]
+    p6 = img.pixels[r+1][c]
+    p7 = img.pixels[r+1][c-1]
+    p8 = img.pixels[r][c-1]
+    p9 = img.pixels[r-1][c-1]
     points = [p2, p3, p4, p5, p6, p7, p8, p9]
     if flag == 1:
-        return neighbor_check(matrix) and zero_one_check(points) and p2p4p6(points) and p4p6p8(points)
-    return neighbor_check(matrix) and zero_one_check(points) and p2p4p8(points) and p2p6p8(points)
+        return neighbor_check(points) and zero_one_check(points) and p2p4p6(points) and p4p6p8(points)
+    return neighbor_check(points) and zero_one_check(points) and p2p4p8(points) and p2p6p8(points)
 
 def zero_one_check(points):
     tempo = None
