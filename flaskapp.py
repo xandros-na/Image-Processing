4edb452cde8ebb7588673fa9a732cb11d87a56f2
Don <don.miguel013@gmail.com>
2015-12-03 16:03:00 -0500

    ian change something

--- flaskapp.py
+++ flaskapp.py
@@ -142,25 +142,22 @@ def recognize_hist():
     f = open("histogram.txt")
     minimum = 0
     index = 0
-
+    temp = [0 for i in range(10)]
     for j, line in enumerate(f):
-        c = 0
         data = line.strip().split(",")
         for i, d in enumerate(data):
-            c += (float(d) - img_vector[i])**2  
+            temp[j // 15] += (float(d) - img_vector[i]) ** 2
 
-        if j == 0:
-            minimum = c
-            number = str(index)
-        else:
-            if c < minimum:
-                minimum = c
-                index = j/15
-                number = str(index)
+    minimum = 0
+    for i in range(10):
+        if temp[i] < temp[minimum]:
+            minimum = i
+        number = str(minimum)
 
     f.close()
 
     return render_template('index.html', filename=filename, number=number, returned=True)
+
 
 @app.route('/recognize/zoning', methods=['POST'])
 def recognize_zone():
@@ -172,25 +169,24 @@ def recognize_zone():
     f = open("zoning.txt")
     minimum = 0
     index = 0
+    temp = [0 for i in range(10)]
 
     for j, line in enumerate(f):
         c = 0
         data = line.strip().split(",")
         for i, d in enumerate(data):
-            c += (float(d) - img_vector[i])**2
+            temp[j // 15] += (float(d) - img_vector[i]) ** 2
 
-        if j == 0:
-            minimum = c
-            number = str(index)
-        else:
-            if c < minimum:
-                minimum = c
-                index = j/15
-                number = str(index)
+    minimum = 0
+    for i in range(10):
+        if temp[i] < temp[minimum]:
+            minimum = i
+        number = str(minimum)
 
     f.close()
 
-
     return render_template('index.html', filename=filename, number=number, returned=True)
+
+
 if __name__ == '__main__':
     manager.run()
